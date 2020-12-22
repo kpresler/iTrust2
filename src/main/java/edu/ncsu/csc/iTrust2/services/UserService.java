dbaedd92dc48c2f63e6101b4d62908bbf15f400b
Kai Presler-Marshall <kpresle@ncsu.edu>
2020-12-22 14:20:32 -0500

    We now respond at  and have hints of landing pages.  Missing some JS pieces

--- src/main/java/edu/ncsu/csc/iTrust2/services/UserService.java
+++ src/main/java/edu/ncsu/csc/iTrust2/services/UserService.java
@@ -1,11 +1,16 @@
 package edu.ncsu.csc.iTrust2.services;
 
+import java.util.NoSuchElementException;
+
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.ExampleMatcher;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Component;
 
+import edu.ncsu.csc.iTrust2.models.User;
 import edu.ncsu.csc.iTrust2.repositories.UserRepository;
 
 @Component
@@ -18,6 +23,23 @@ public class UserService extends Service {
     @Override
     protected JpaRepository getRepository () {
         return repository;
+    }
+
+    public User findByName ( final String username ) {
+        final User user = new User();
+        user.setUsername( username );
+
+        final ExampleMatcher matcher = ExampleMatcher.matching().withMatcher( "username",
+                ExampleMatcher.GenericPropertyMatchers.exact() );
+
+        final Example<User> example = Example.of( user, matcher );
+
+        try {
+            return repository.findOne( example ).get();
+        }
+        catch ( final NoSuchElementException nsee ) {
+            return null;
+        }
     }
 
 }
