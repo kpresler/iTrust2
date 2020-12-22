2eaeb609c7e8ed3fd8204b1fbc4285b085e47772
Kai Presler-Marshall <kpresle@ncsu.edu>
2020-12-22 14:02:36 -0500

    Remove all CoffeeMaker functionality, don't need it any more

--- src/main/java/edu/ncsu/csc/iTrust2/repositories/RecipeRepository.java
+++
@@ -1,9 +0,0 @@
-package edu.ncsu.csc.iTrust2.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import edu.ncsu.csc.iTrust2.models.Recipe;
-
-public interface RecipeRepository extends JpaRepository<Recipe, Long> {
-
-}
