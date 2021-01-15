e8cf4dda663fefb8b00399fe5dfbce59c1934610
Kai Presler-Marshall <kpresle@ncsu.edu>
2021-01-14 20:31:12 -0500

    Tests for OfficeVisit + BHM frontend

--- src/test/java/edu/ncsu/csc/iTrust2/cucumber/CommonStepDefs.java
+++ src/test/java/edu/ncsu/csc/iTrust2/cucumber/CommonStepDefs.java
@@ -2,21 +2,27 @@ package edu.ncsu.csc.iTrust2.cucumber;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import edu.ncsu.csc.iTrust2.forms.UserForm;
+import edu.ncsu.csc.iTrust2.models.Hospital;
 import edu.ncsu.csc.iTrust2.models.Patient;
 import edu.ncsu.csc.iTrust2.models.Personnel;
 import edu.ncsu.csc.iTrust2.models.User;
 import edu.ncsu.csc.iTrust2.models.enums.Role;
+import edu.ncsu.csc.iTrust2.models.enums.State;
+import edu.ncsu.csc.iTrust2.services.HospitalService;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.When;
 
 public class CommonStepDefs extends CucumberTest {
 
+    @Autowired
+    private HospitalService hospitalService;
+
     @Given ( "An Admin exists in iTrust2" )
     public void adminExists () {
         final User u = new Personnel( new UserForm( "admin", "123456", Role.ROLE_ADMIN, 1 ) );
-
         userService.save( u );
 
     }
@@ -24,16 +30,20 @@ public class CommonStepDefs extends CucumberTest {
     @Given ( "A Patient exists in iTrust2" )
     public void patientExists () {
         final User u = new Patient( new UserForm( "patient", "123456", Role.ROLE_PATIENT, 1 ) );
-
         userService.save( u );
     }
 
     @Given ( "An HCP exists in iTrust2" )
     public void HCPExists () {
         final User u = new Personnel( new UserForm( "hcp", "123456", Role.ROLE_HCP, 1 ) );
-
         userService.save( u );
 
+    }
+
+    @Given ( "A hospital exists in iTrust2" )
+    public void hospitalExists () {
+        final Hospital hospital = new Hospital( "iTrust Test Hospital 2", "Bialystok", "10101", State.NJ.toString() );
+        hospitalService.save( hospital );
     }
 
     /**
